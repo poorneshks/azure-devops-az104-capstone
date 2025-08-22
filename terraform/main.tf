174c3e8ee8cc3cba673e73ab11af95acfa5576b4
poorneshks <132078229+poorneshks@users.noreply.github.com>
2025-08-22 18:49:10 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,4 +44,6 @@ resource "azurerm_log_analytics_workspace" "law" {
 resource "azurerm_application_insights" "appi" {
   name                = "${var.project_name}-appi"
   location            = azurerm_resource_group.rg.location
-  resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
