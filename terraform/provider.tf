1e6e2d0cbd3aa63919678c41755d463132c645d2
poorneshks <132078229+poorneshks@users.noreply.github.com>
2025-08-22 19:53:00 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,11 +5,16 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.100.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.0"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
+
   subscription_id = "3752d2f8-6ef2-4533-8ac8-3f4a0e631329"
   tenant_id       = "ce4136a6-8dad-47c5-9cdc-ddc03da55699"
   client_id       = "233070f8-f525-4899-a2bb-a1b5f0618304"
